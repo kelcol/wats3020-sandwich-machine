23f8a4c3a7bc39258b0c1d4ff77ac1b6fa7d7e8a
kelcol <kelly.colht@fastmail.fm>
2018-01-21 10:32:09 -0800

    Fixed pricing calculations

--- js/main.js
+++ js/main.js
@@ -1,48 +1,18 @@
 /* JavaScript for WATS 3020 Sandwich Machine Assignment */
 
-// Step One ///////////////////////////////////////////////////////////
-//
-// Gather data from the user using the prompt() command.
-// Users will fill in each prompt with text.
-// Provide examples where it may help (e.g. show a comma-separated list of
-// toppings to indicate how to specify toppings, etc.)
-//
-
-
-// Create a function to "decimalate numbers"
-function decimalate(x) {
-  return Number.parseFloat(x).toFixed(2);
-};
-
-
-// TODO: Prompt the user for what kind of bread they would like.
-// Ideally, that would look something like: "What kind of bread (white, wheat, flat)?"
+// Prompt the user for what kind of bread they would like.
 let bread = prompt("What kind of bread would you like?");
 
-// TODO: Prompt the user for what kind of meat(s) they would like.
-// Indicate they should separate multiple items with a comma:
-// "What kind of meat? (Separate meats with a comma if you would like more than one.)"
+// Prompt the user for what kind of meat(s) they would like.
 let meatString =  prompt("What sorts of meat would you like? You can have more than one, just separate with commas so our meat guy doesn't spend all day looking for 'chicken head cheese'.");
 
-
-// TODO: Prompt the user for what kind of toppings they would like.
-// We expect this to be multiple, so ask them to provide you with a
-// comma-separated list using a user friendly prompt.
+// Prompt the user for what kind of toppings they would like.
 let toppingString =  prompt("What sorts of toppings would you like? You can have more than one, just separate with commas so our topping gal doesn't spend all day looking for 'anchovy tomatoes'.");
 
-
-// TODO: Prompt the user for what kind of condiments they would like.
-// Again, we should expect a comma-separated list if items here.
+// Prompt the user for what kind of condiments they would like.
 let condimentString =  prompt("What sorts of condiments would you like? You can have more than one, just separate with commas so the condiment folk don't spend all day looking for 'banana pepper oil'.");
 
-
-// Step Two ////////////////////////////////////////////////////////////
-//
-// Process all the input to calculate values for the user's order. The named
-// values are initialized to `null`. Change that so they equal the proper value.
-//
-// The `prices` object below specifies the prices for each thing.
-
+// Set base pricing for sandwich and additions
 let prices = {
     sandwich: 5, // Base price for a sandwich is $5, includes bread
     meat: 1, // Each kind of meat on a sandwich costs $1
@@ -50,40 +20,35 @@ let prices = {
     condiment: 0.25 // Each condiment costs $0.25
 };
 
-let sandwichCost = decimalate(prices.sandwich);
-
-// TODO: Convert order information from Strings to Arrays.
-
+// Convert order information from Strings to Arrays.
 let meatArray = meatString.split(',');
 let toppingArray = toppingString.split(',');
 let condimentArray = condimentString.split(',');
 
-// TODO: Calculate cost for meat, toppings, and condiments.
-// This requires you to determine the length of each Array you just made
-// and multiply out the costs. You will need to refer to the attributes of the
-// `prices` object in order to calculate these costs.
+// Calculate cost for meat, toppings, and condiments.
+// Convert strings to numbers using the unary + operators
+// http://xkr.us/articles/javascript/unary-add/)
+let sandwichCost = +prices.sandwich.toFixed(2);
+let meatCost = (+meatArray.length * +prices.meat).toFixed(2);
+let toppingCost = (+toppingArray.length * +prices.topping).toFixed(2);
+let condimentCost = (+condimentArray.length * +prices.condiment).toFixed(2);
 
-let meatCost = decimalate(meatArray.length * prices.meat);
-let toppingCost = decimalate(toppingArray.length * prices.topping);
-let condimentCost = decimalate(condimentArray.length * prices.condiment);
+// Combine the costs of each part of the sandwich to get the subtotal.
+let subTotal = (+sandwichCost + +meatCost + +toppingCost + +condimentCost).toFixed(2);
 
-// TODO: Combine the costs of each part of the sandwich to get the subtotal.
-let subTotal = decimalate(sandwichCost + meatCost + toppingCost + condimentCost);
-
-// TODO: Calculate the tax owed using the waStateTaxRate.
+// Calculate the tax owed using the waStateTaxRate.
 let waStateTaxRate = 0.065;
-let orderTax = decimalate(subTotal * waStateTaxRate);
+let orderTax = (+subTotal * +waStateTaxRate).toFixed(2);
 
-// TODO: Calculate `totalPrice` by adding `subtotal` and `orderTax`.
-let totalPrice = decimalate(subTotal + orderTax);
+// Calculate `totalPrice` by adding `subtotal` and `orderTax`.
+let totalPrice = (+subTotal + +orderTax).toFixed(2);
 
-// Step Three //////////////////////////////////////////////////////////
-//
-// TODO: Now that we've calculated all the values, insert them into this
-// template literal using proper expression tags. Note that we must provide
-// all of the info the user gave us to confirm the order, and then we must also
-// provide the cost information so the user understands their bill.
+// Calculate gratuity
+let okay = (+subTotal * .10).toFixed(2);
+let good = (+subTotal * .15).toFixed(2);
+let great = (+subTotal * .20).toFixed(2);
 
+// Insert variables created into template literal
 let receiptTemplate = `
     <p>SANDWICH ORDER</p>
     <p>Bread: ${bread}</p>
@@ -100,6 +65,11 @@ let receiptTemplate = `
     <p class="text-right">Tax: $${orderTax}</p>
     <p class="text-right">--------</p>
     <p class="text-right">Total: $${totalPrice}</p>
+	<p>---------------------</p>
+	GRATUITY:
+	<p>Okay (10%): $${okay}</p>
+	<p>Good (15%): $${good}</p>
+	<p>Great (20%): $${great}</p>
 `
 
 ///////////////////////////////////////////////////////////////////////
