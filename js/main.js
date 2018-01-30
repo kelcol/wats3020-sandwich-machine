1f771a159047764789b186928c0fd0c4e223b81a
kelcol <colhk@seattleu.edu>
2018-01-29 20:35:16 -0800

    fixed rounding issues

--- js/main.js
+++ js/main.js
@@ -26,27 +26,25 @@ let toppingArray = toppingString.split(',');
 let condimentArray = condimentString.split(',');
 
 // Calculate cost for meat, toppings, and condiments.
-// Convert strings to numbers using the unary + operators
-// http://xkr.us/articles/javascript/unary-add/)
-let sandwichCost = +prices.sandwich.toFixed(2);
-let meatCost = (+meatArray.length * +prices.meat).toFixed(2);
-let toppingCost = (+toppingArray.length * +prices.topping).toFixed(2);
-let condimentCost = (+condimentArray.length * +prices.condiment).toFixed(2);
+let sandwichCost = prices.sandwich;
+let meatCost = meatArray.length * prices.meat;
+let toppingCost = toppingArray.length * prices.topping;
+let condimentCost = condimentArray.length * prices.condiment;
 
 // Combine the costs of each part of the sandwich to get the subtotal.
-let subTotal = (+sandwichCost + +meatCost + +toppingCost + +condimentCost).toFixed(2);
+let subTotal = sandwichCost + meatCost + toppingCost + condimentCost;
 
 // Calculate the tax owed using the waStateTaxRate.
 let waStateTaxRate = 0.065;
-let orderTax = (+subTotal * +waStateTaxRate).toFixed(2);
+let orderTax = subTotal * waStateTaxRate;
 
 // Calculate `totalPrice` by adding `subtotal` and `orderTax`.
-let totalPrice = (+subTotal + +orderTax).toFixed(2);
+let totalPrice = (subTotal + orderTax).toFixed(2);
 
 // Calculate gratuity
-let okay = (+subTotal * .10).toFixed(2);
-let good = (+subTotal * .15).toFixed(2);
-let great = (+subTotal * .20).toFixed(2);
+let okay = (subTotal * .10).toFixed(2);
+let good = (subTotal * .15).toFixed(2);
+let great = (subTotal * .20).toFixed(2);
 
 // Insert variables created into template literal
 let receiptTemplate = `
@@ -56,13 +54,13 @@ let receiptTemplate = `
     <p>Toppings: ${toppingArray}</p>
     <p>Condiments: ${condimentArray}</p>
     <p>---------------------</p>
-    <p class="text-right">Sandwich: $${sandwichCost}</p>
-    <p class="text-right">Meat: $${meatCost}</p>
-    <p class="text-right">Toppings: $${toppingCost}</p>
-    <p class="text-right">Condiments: $${condimentCost}</p>
+    <p class="text-right">Sandwich: $${(sandwichCost).toFixed(2)}</p>
+    <p class="text-right">Meat: $${(meatCost).toFixed(2)}</p>
+    <p class="text-right">Toppings: $${(toppingCost).toFixed(2)}</p>
+    <p class="text-right">Condiments: $${(condimentCost).toFixed(2)}</p>
     <p class="text-right">--------</p>
-    <p class="text-right">Subtotal: $${subTotal}</p>
-    <p class="text-right">Tax: $${orderTax}</p>
+    <p class="text-right">Subtotal: $${(subTotal).toFixed(2)}</p>
+    <p class="text-right">Tax: $${(orderTax).toFixed(2)}</p>
     <p class="text-right">--------</p>
     <p class="text-right">Total: $${totalPrice}</p>
 	<p>---------------------</p>
